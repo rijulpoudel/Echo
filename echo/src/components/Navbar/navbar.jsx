96fdb3d7ca8160e95ba5ba235b304b4b64c3a204
mariskarai <rmariskaa38@gmail.com>
2025-10-12 17:35:26 -0500

    navbar component

---
+++ echo/src/components/Navbar/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css' //this is to bring in the navbar.css page in react
+
+const Navbar = () => {
+  return (
+    <div className = 'navbar' >
+
+
+
+    </div>
+  )
+}
+
+export default Navbar
